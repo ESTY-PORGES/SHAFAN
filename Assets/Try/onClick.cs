ab4d72f55ba220a4201ed40425f010f7c1bda443
EttyBlack <6166887@gmail.com>
2021-11-16 12:54:23 +0200

    Add scene and try code

---
+++ Assets/Try/onClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class onClick : MonoBehaviour
+{
+
+    Dictionary<Button, AudioClip> dict = new Dictionary<Button, AudioClip>();
+
+  
+
+    public void Play()
+    {
+
+        //print("buttons");
+
+        ////for (int i = 0; i < dict.Count; i++)
+        ////{
+
+        //dict.Add(buttons[0], audioClips[0]);
+
+        //}
+
+    }
+
+}
+
+
